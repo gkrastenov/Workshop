b9001862247c1314bac1b96964826b014bc90aa3
Georgi Krastenov <35624842+gkrastenov@users.noreply.github.com>
2021-03-13 14:36:15 +0200

    initial commit Currency Converter

---
+++ CurrencyConvertor/CurrencyConvertor.Models/Rate.cs
@@ -0,0 +1,14 @@
+﻿namespace CurrencyConvertor.Models
+{
+    public class Rate
+    {
+        public Rate(string name, decimal value)
+        {
+            this.Name = name;
+            this.Value = value;
+        }
+
+        public string Name { get; private set; }
+        public decimal Value { get; private set; }
+    }
+}
